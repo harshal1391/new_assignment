a78369b92c5523519708b74e22d6cc3c4fe6be26
Harshal Deshpande <harshaldeshpande1391@gmail.com>
2021-10-14 11:07:38 +0530

    Cors added for frontend connectivity

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const db = require("./config/database");
 const APIError = require("./helpers/APIError");
 const httpStatus = require("http-status");
 const path = require("path");
+const cors = require("cors");
 const port = process.env.PORT || 8001;
 
 const app = express();
 app.set("views", path.join(__dirname));
 app.use(bodyParser.urlencoded({ limit: "15gb", extended: true }));
 app.use(bodyParser.json({ limit: "15gb" }));
+app.use(cors());
 
 db.connection().then((database) => {
   module.exports = database;
